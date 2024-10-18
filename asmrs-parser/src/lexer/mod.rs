f19ba14061246e4c9c9b8a2f69b479fe8dd35dca
Hqnnqh <hannah@diefluchs.at>
2024-10-18 09:14:38 +0200

    feat(lexer): implement syntax error struct

--- asmrs-parser/src/lexer/mod.rs
+++ asmrs-parser/src/lexer/mod.rs
@@ -1,10 +1,9 @@
 use crate::lexer::token::Token;
-use std::vec::Vec;
+use std::{error::Error, fmt::Display, vec::Vec};
 
 pub mod token;
 
-// todo: return Result<Vec<Token>, SyntaxError> instead
-pub fn tokenize(input: String) -> Vec<Token> {
+pub fn tokenize(input: String) -> Result<Vec<Token>, SyntaxError> {
     let mut tokens = Vec::default();
     let mut line_index = 0usize;
     let mut char_index = 0usize;
@@ -31,13 +30,17 @@ pub fn tokenize(input: String) -> Vec<Token> {
                         continue;
                     }
                 }
-                todo!("Syntax Error Handling: Comment invalid")
+                return Err(SyntaxError::new(
+                    "Expected '/'. Invalid comment syntax.".to_string(),
+                    line_index,
+                    char_index,
+                ));
             }
             _ => todo!("Implement remaining syntactical patterns"),
         }
     }
 
-    tokens
+    Ok(tokens)
 }
 
 #[test]
@@ -45,6 +48,47 @@ fn tokenize_comment() {
     let input = "// Hello, this is a comment\n".to_string();
 
     let output = tokenize(input);
-
-    assert!(output.is_empty());
+    assert!(output.is_ok());
+    assert!(output.unwrap().is_empty());
 }
+
+#[test]
+fn tokenize_comment_syntax_error() {
+    let input = "// Hello, this is a valid comment\n/ This is an invalid comment\n".to_string();
+
+    let output = tokenize(input);
+    assert!(output.is_err());
+    let output = output.unwrap_err();
+    assert_eq!(output.char_index, 0);
+    assert_eq!(output.line_index, 1);
+}
+
+#[derive(Clone, Debug)]
+pub struct SyntaxError {
+    message: String,
+    line_index: usize,
+    char_index: usize,
+}
+
+impl SyntaxError {
+    /// Creates a new Syntax Error with the given message, line number and column number.
+    pub fn new(message: String, line_index: usize, char_index: usize) -> SyntaxError {
+        Self {
+            message,
+            line_index,
+            char_index,
+        }
+    }
+}
+
+impl Display for SyntaxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Syntax Error: at line: {}, column: {}: {}",
+            self.line_index, self.char_index, self.message
+        )
+    }
+}
+
+impl Error for SyntaxError {}
